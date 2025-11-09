a015c65f002bd36b493e2303355c269c2d32dfa5
badavathsaikiran1310 <kirangag33@gmail.com>
2025-11-09 08:57:29 -0800

    First commit

---
+++ first.js
@@ -0,0 +1,69 @@
+// console.log("saikiran");
+// console.log("saiiran");
+// console.log("saian");
+
+// let fullname = "AKHI NAYAK";
+// let age = 21;
+// age =22;
+// age =20;
+
+// const pi =3.14;
+// console.log(pi);
+// college = true;
+// x = null;
+// y = undefined;
+// console.log(x);
+// console.log(y);
+// console.log(fullname);
+// console.log(age);
+// console.log(college);
+
+
+// let x = BigInt("123");
+// let y = Symbol("HELLO");
+
+
+// const student = {
+//     fullname : "AKHI NAYAK",
+//     age :20,
+//     cgpa :8.8,
+//     ispass : true
+// }
+
+// console.log(student["age"])
+// console.log(student.age+1)
+
+
+// let a = 6;
+// let b = 4;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+
+// console.log(a++);
+// console.log(a);
+
+// console.log(--a);
+// console.log(a);
+
+// console.log(b++);
+// console.log(b);
+
+// console.log(--b);
+
+
+let a = 6;
+let b = 4;
+
+console.log(a+=1);
+console.log(a===8);
+console.log(a!==8);
+
+
+
+
